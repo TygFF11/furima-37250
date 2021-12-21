7a7d058e67c11c27f7c1f4709fee47922b0f627f
TygFF11 <kh091216@icloud.com>
2021-12-21 13:12:14 +0900

    itemsモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,40 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_12_21_000324) do
 
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "item_name", null: false
+    t.text "item_describe", null: false
+    t.integer "item_price", null: false
+    t.integer "item_category_id", null: false
+    t.integer "item_show_id", null: false
+    t.integer "item_delivary_id", null: false
+    t.integer "perfecture_id", null: false
+    t.integer "item_day_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "nickname", default: "", null: false
+    t.string "last_name", default: "", null: false
+    t.string "last_name_kana", default: "", null: false
+    t.string "first_name", default: "", null: false
+    t.string "first_name_kana", default: "", null: false
+    t.date "birthday", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "items", "users"
 end
