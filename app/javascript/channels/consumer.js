a75f6ec1d9ab842accaf674055a9823936e4e63d
TygFF11 <kh091216@icloud.com>
2021-12-28 15:57:02 +0900

    javascript 手数料・利益

--- app/javascript/channels/consumer.js
+++ app/javascript/channels/consumer.js
@@ -1,16 +1,3 @@
 import { createConsumer } from "@rails/actioncable"
 export default createConsumer()
 
-// function consumer (){
-//   const sale = document.getElementById("item-price") 
-//   sale.addEventListener("oninput", function(){
-    // ("add-tax-price")
-    // item_price * 0.9
-    // ("profit")
-    // item_price * 0.1
-//   });
-// });
-
-
-
-// window.addEventListener('load', consumer);
