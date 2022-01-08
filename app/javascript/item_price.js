3314dfe090674d960c0f4195ccc801abed10d495
TygFF11 <kh091216@icloud.com>
2022-01-08 16:31:04 +0900

    手数料・修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,19 +1,16 @@
 function consumer (){
   const priceInput = document.getElementById("item-price") 
-
-  const addTaxDom = document.getElementById("add-tax-price")
-  const profit = document.getElementById("profit")
-
+  
   priceInput.addEventListener("input", function(){
     const inputValue = priceInput.value
-
-    const commissionCalculation = Math.floor( inputValue / 10 )
-    const profitCalculation = inputValue - commissionCalculation
     
-    addTaxDom.innerHTML = commissionCalculation
-    profit.innerHTML = profitCalculation
+    const addTaxDom = document.getElementById("add-tax-price")
+    addTaxDom.innerHTML = Math.round( priceInput.value / 10 )
+    
+    const profit = document.getElementById("profit")
+    profit.innerHTML = priceInput.value - Math.round( priceInput.value / 10 )
+    
   })
 }
-
 
 window.addEventListener('load', consumer)
