acf96e0f87a73e1af444890fb216b7e0879776d9
TygFF11 <kh091216@icloud.com>
2022-01-14 18:12:07 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,6 @@ class User < ApplicationRecord
   has_many :orders
 
   def self.from_omniauth(auth)
-    binding.pry
     sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
     user = User.where(email: auth.info.email).first_or_initialize(
       nickname: auth.info.name,
