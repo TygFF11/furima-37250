2fa5fce4428c3adbc8dd7ac33facb0a927216b73
TygFF11 <kh091216@icloud.com>
2022-01-18 19:11:10 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,10 +20,6 @@ class User < ApplicationRecord
     user = User.where(email: auth.info.email).first_or_initialize(
       nickname: auth.info.name,
       email: auth.info.email
-      # last_name: auth.info.last_name
-      # last_name_kana: auth.info.last_name_kana
-      # first_name: auth.info.first_name
-      # first_name_kana: auth.info.first_name_kana
     )
     if user.persisted?
       sns.user = user
